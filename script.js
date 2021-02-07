bbc0e9b19c5703063784781efa336cd620049d80
Jahid Hasan <jahidjjaa@gmail.com>
2021-02-07 08:02:11 +0600

    searcing meals  new design

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const searchItem = document.getElementById('food-input');
 const searchBtn = document.getElementById('search-btn');
-const ingredientBox =document.getElementById('ingredient')
+const ingredientBox = document.getElementById('ingredient');
 const foodList = document.getElementById('food-list');
 
 //displaying ingredient 
@@ -8,28 +8,35 @@ const foodsIngredient =(mealName)=>{
     ingredientBox.style.display = 'block'
     fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+mealName)
     .then(res =>res.json())
-    .then(ingredient =>{
-     let mealsIngredient =ingredient.meals[0]
+    .then(ingredients =>{
+     let mealsIngredient =ingredients.meals[0];
      ingredientBox.innerHTML =`<img src="${mealsIngredient.strMealThumb}" class="img-fluid" alt="">
-     <h5 class="food-names">${mealName}</h5>
-     <h5>Ingredients</h5>
-     <ul><li>${mealsIngredient.strIngredient1}</li>
-     <li>${mealsIngredient.strIngredient2}</li>
-     <li>${mealsIngredient.strIngredient3}</li>
-     <li>${mealsIngredient.strIngredient4}</li>
-     <li>${mealsIngredient.strIngredient5}</li>
-     <li>${mealsIngredient.strIngredient6}</li></ul>`
-    } )      
+     <h3 class="food-names">${mealName}</h3>
+     <h4> Ingredients</h4>
+     <ul>
+     <li>${mealsIngredient.strIngredient1} ${mealsIngredient.strMeasure1}</li>
+     <li>${mealsIngredient.strIngredient2} ${mealsIngredient.strMeasure2}</li>
+     <li>${mealsIngredient.strIngredient3} ${mealsIngredient.strMeasure3}</li>
+     <li>${mealsIngredient.strIngredient4} ${mealsIngredient.strMeasure4}</li>
+     <li>${mealsIngredient.strIngredient5} ${mealsIngredient.strMeasure5}</li>
+     <li>${mealsIngredient.strIngredient6} ${mealsIngredient.strMeasure6}</li>
+     <li>${mealsIngredient.strIngredient3} ${mealsIngredient.strMeasure7}</li>
+     <li>${mealsIngredient.strIngredient4} ${mealsIngredient.strMeasure8}</li>
+     <li>${mealsIngredient.strIngredient5} ${mealsIngredient.strMeasure9}</li>
+     <li>${mealsIngredient.strIngredient6} ${mealsIngredient.strMeasure10}</li>
+     
+     </ul>`
+    })      
 }
 
 //displaying foods template
 const displayFoods = (foods) =>{
     foodList.innerHTML='';
-    ingredientBox.style.display = 'none'
+    ingredientBox.style.display = 'none';
 
     let allFood = foods.meals;
     if (allFood == null) {
-        foodList.innerHTML =`   <h3> No result found for ${searchItem.value}</h3>`
+        foodList.innerHTML =`   <h2> No result found for ${searchItem.value}</h2>`
     } else {
         allFood.forEach(food => {
         let {strMeal} = food;
@@ -37,7 +44,7 @@ const displayFoods = (foods) =>{
         let foodPlate = document.createElement('div')
 
         foodPlate.id ='food-plate';  
-        foodPlate.innerHTML = `<img src="${strMealThumb}" class="foods-img img-fluid" alt="">
+        foodPlate.innerHTML = `<img src="${strMealThumb}" class="img-fluid" alt="">
         <h3 class="food-names">${strMeal}</h3>`
         foodList.appendChild(foodPlate); 
         
