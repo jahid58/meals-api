699caaa0799475620a2beb3cf4ee3f26f7e21575
Jahid Hasan <jahidjjaa@gmail.com>
2021-02-08 12:56:04 +0600

    revision

--- script.js
+++ script.js
@@ -7,6 +7,37 @@ const ingredientBox = document.getElementById('meal-details');
 const ingredientUl = document.getElementById('ingredients');
 const measureUl = document.getElementById('measure')
 
+//fetching api for meals
+const displayFoodList =()=>{  
+    foodList.innerHTML='';
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+searchItem.value)
+    .then(res=>res.json())
+    .then(meals=>displayFoods(meals))
+}
+
+searchBtn.addEventListener('click',displayFoodList);
+
+//displaying foods template
+const displayFoods = (foods) =>{
+    ingredientBox.style.display = 'none';
+
+    let allFood = foods.meals;
+    if (allFood == null || searchItem.value == '' ) {
+        foodList.innerHTML = `   <h2> No result found for ${searchItem.value}</h2>`
+     } else {
+        allFood.forEach(food => {
+        let {strMeal} = food;
+        let {strMealThumb} = food;
+        let foodPlate = document.createElement('div')
+        foodPlate.id ='food-plate';  
+        foodPlate.innerHTML = `<img src="${strMealThumb}" class="img-fluid" alt="">
+        <h3 class="food-names">${strMeal}</h3>`
+        foodList.appendChild(foodPlate); 
+        
+        foodPlate.addEventListener('click',()=>foodsIngredient(strMeal))
+     });
+  }
+}
 
 //displaying ingredient function
 const foodsIngredient =(mealName)=>{
@@ -37,37 +68,9 @@ const foodsIngredient =(mealName)=>{
    })   
 }          
 
-//displaying foods template
-const displayFoods = (foods) =>{
-    ingredientBox.style.display = 'none';
 
-    let allFood = foods.meals;
-    if (allFood == null || searchItem.value == '' ) {
-        foodList.innerHTML = `   <h2> No result found for ${searchItem.value}</h2>`
-     } else {
-        allFood.forEach(food => {
-        let {strMeal} = food;
-        let {strMealThumb} = food;
-        let foodPlate = document.createElement('div')
-        foodPlate.id ='food-plate';  
-        foodPlate.innerHTML = `<img src="${strMealThumb}" class="img-fluid" alt="">
-        <h3 class="food-names">${strMeal}</h3>`
-        foodList.appendChild(foodPlate); 
-        
-        foodPlate.addEventListener('click',()=>foodsIngredient(strMeal))
-     });
-  }
-}
 
-//fetching api for meals
-const displayFoodList =()=>{  
-    foodList.innerHTML='';
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+searchItem.value)
-    .then(res=>res.json())
-    .then(meals=>displayFoods(meals))
-}
 
-searchBtn.addEventListener('click',displayFoodList);
 
 
 
